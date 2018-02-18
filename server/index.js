4f7fc5dd726c79f429a55fb33880f45009289247
Ko-Hsin Chu <chukohsin@gmail.com>
2018-02-18 17:35:01 -0500

    took out engines

--- server/index.js
+++ server/index.js
@@ -84,7 +84,7 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(process.env.PORT || PORT, () => console.log(`Mixing it up on port ${PORT}`))
+  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 
   // set up our socket control center
   const io = socketio(server)

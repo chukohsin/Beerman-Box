a66767f35808158b18cd1947cee5a1c52277c6e4
Ko-Hsin Chu <chukohsin@gmail.com>
2018-02-18 15:48:32 -0500

    db changes

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,45 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/alcoholordiedb', {
-    logging: false
-  }
+
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+};
+
+
+let db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/alcoholordiedb', { logging: false, operatorsAliases }
 )
 module.exports = db
